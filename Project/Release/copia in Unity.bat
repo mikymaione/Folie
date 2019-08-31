0fbb1d9cad910114179ee31f598fd8d3a1d1957a
Michele Maione <mikymaione@hotmail.it>
2019-08-31 16:51:46 +0200

    bat

--- Project/Release/copia in Unity.bat
+++ Project/Release/copia in Unity.bat
@@ -1,2 +1,2 @@
-copy Folie.dll "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
-copy Folie.pdb "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
+copy Folie.dll "..\Volleyball\Assets\Plugins"
+copy Folie.pdb "..\Volleyball\Assets\Plugins"
