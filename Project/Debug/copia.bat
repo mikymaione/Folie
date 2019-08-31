a75a21ea184e2e7455fa85df8d775268cc1aae7d
Michele Maione <mikymaione@hotmail.it>
2019-08-31 16:51:10 +0200

    bat

--- Project/Debug/copia.bat
+++
@@ -1,2 +0,0 @@
-copy Folie.dll "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
-copy Folie.pdb "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
