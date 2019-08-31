81cf004b296ea75682e80eddcdceb074ed77ab20
Michele Maione <mikymaione@hotmail.it>
2019-08-31 16:49:26 +0200

    bat

---
+++ Project/Debug/copia in Unity.bat
@@ -0,0 +1,2 @@
+copy Folie.dll "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
+copy Folie.pdb "C:\Users\mikym\Documents\UniMi\2S\AI\Folie\Project\Volleyball\Assets\Plugins"
